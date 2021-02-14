102ef6779da8263011d2d618ec8ba3032e37274c
Alex Ivanov <alexivanovrus@gmail.com>
2021-02-14 14:48:06 +0300

    Added commands

--- src/Application.java
+++ src/Application.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedHashSet;
-import java.util.Scanner;
 import java.util.Set;
 
 public class Application {
